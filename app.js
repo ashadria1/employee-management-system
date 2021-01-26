1f143431d5fcf27c670912118fa108b89f7c804c
ashadria <ashadria@gmail.com>
2021-01-25 19:41:33 -0500

    updating SQL, adding functions

---
+++ app.js
@@ -0,0 +1,172 @@
+/* var inquirer = require("inquirer");
+var connection = require('./connection');
+
+/* * The command-line application should allow users to:
+
+  * View departments, roles, employees
+  
+  * Add departments, roles, employees
+
+  * Update employee roles */
+  
+  const viewOptions = [
+    "View Departments",
+    "View Roles",
+    "View Employees",
+    "Add Departments",
+    "Add Roles",
+    "Add Employees",
+    "Update Employee Roles",
+    "exit"
+]
+
+const employeeOptions = [
+    "Thewolf OfWallStreet",
+    "Kevin Flynn",
+    "Itzhak Stern",
+    "Perry Mason",
+    "Boss Hogg",
+    "Tom Sawyer",
+    "Huck Finn",
+    "Bilbo Baggins",
+    "exit"
+];
+
+const updateOptions = [
+    "First Name",
+    "Last Name",
+    "Role",
+    "exit"
+];
+
+runSearch();
+
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: viewOptions
+        })
+        .then(function (answer) {
+/* 
+            const viewOptions = [
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Add Departments",
+                "Add Roles",
+                "Add Employees",
+                "exit" */
+
+                switch (answer.action) {
+                case viewOptions[0]:
+                    departmentView();
+                    break;
+
+                case viewOptions[1]:
+                    roleView();
+                    break;
+
+                case viewOptions[2]:
+                    employeeView();
+                    break;
+
+                case viewOptions[4]:
+                    departmentAdd();
+                    break;
+
+                case viewOptions[5]:
+                    roleAdd();
+                    break;
+
+                case viewOptions[6]:
+                    employeeAdd();
+                    break;
+
+                case viewOptions[7]:
+                    employeeUpdate();
+                    break;
+        
+                case viewOptions[8]:
+                    connection.end();
+                    break
+              }
+        })
+}
+
+function departmentView() { 
+    var sqlStr = "SELECT * FROM department";
+    connection.query(sqlStr, function (err, result) {
+        if (err) throw err;
+        console.table(result)
+        runSearch();
+    })
+}
+function employeeView() {
+    var sqlStr = "SELECT * FROM employee ";
+    sqlStr += "LEFT JOIN role ";
+    sqlStr += "ON employee.role_id = role.id"
+    connection.query(sqlStr, function (err, result) {
+        if (err) throw err;
+        console.table(result)
+        runSearch();
+    })
+}
+function roleView() {
+    var sqlStr = "SELECT * FROM role";
+    connection.query(sqlStr, function (err, result) {
+        if (err) throw err;
+        console.table(result)
+        runSearch();
+    })
+}
+
+
+// Adding functions......................................................
+/* 
+function departmentAdd() {
+    var sqlStr = "SELECT * FROM department";
+    connection.query(sqlStr, function (err, result) {
+        if (err) throw err;
+
+        console.table(result)
+        runSearch();
+    })
+}
+function employeeAdd() {
+    var sqlStr = "SELECT * FROM employee ";
+    sqlStr += "LEFT JOIN role ";
+    sqlStr += "ON employee.role_id = role.id"
+    connection.query(sqlStr, function (err, result) {
+        if (err) throw err;
+
+        console.table(result)
+        runSearch();
+    })
+}
+function roleAdd() {
+    var sqlStr = "SELECT * FROM role";
+    connection.query(sqlStr, function (err, result) {
+        if (err) throw err;
+
+        console.table(result)
+        runSearch();
+    })
+} */
+
+// "Update Employee Roles"......................................................................................
+const updateEmployee = () => {
+
+    function runUpdateSearch() {
+        inquirer
+            .prompt({
+                name: "action",
+                type: "list",
+                message: "Which employee do you want to update?",
+                choices: employeeOptions
+            })
+    }
+    runUpdateSearch();  
+}; 
---
+++ employee-schema.sql
@@ -0,0 +1,118 @@
+DROP DATABASE IF EXISTS Employee_Managament;
+
+CREATE DATABASE Employee_Managament;
+
+USE Employee_Managament;
+
+-- Design the following database schema containing three tables:
+
+-- * **department**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **name** - VARCHAR(30) to hold department name
+
+CREATE TABLE department(
+id INTEGER auto_increment NOT NULL,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
+);
+
+-- Add dummy data to the department table...
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+INSERT INTO department (name)
+VALUES ("Manager");
+
+-- Test department table...
+
+SELECT * FROM department;
+
+-- * **role**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
+
+CREATE TABLE role(
+id INTEGER auto_increment NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER NOT NULL,
+CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(id),
+PRIMARY KEY(id)
+);
+
+-- Add dummy data to the role table...
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 65000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 55000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 73000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 60000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal", 70000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 75000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Gopher", 5000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Flunkie", 5000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Intern", 500, 2);
+
+-- Test the role table...
+
+SELECT * FROM role;
+
+-- * **employee**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **first_name** - VARCHAR(30) to hold employee first name
+--   * **last_name** - VARCHAR(30) to hold employee last name
+--   * **role_id** - INT to hold reference to role employee has
+--   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+
+CREATE TABLE employee(
+id INTEGER auto_increment NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(id),
+manager_id INTEGER ,
+CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id),
+PRIMARY KEY(id)
+);
+
+-- Add dummy data to employee table...
+
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Thewolf", "OfWallStreet", 1); 
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Kevin", "Flynn", 2);
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Itzhak", "Stern", 3); 
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Perry", "Mason", 4);
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Boss", "Hogg", 5);
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Tom", "Sawyer", 6);
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Huck", "Finn", 7);
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Bilbo", "Baggins", 8);
+
+-- Test employee table...
+
+SELECT * FROM employee;
